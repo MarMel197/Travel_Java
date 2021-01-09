64ac94b788600b74dc2093288faa786248b23b28
Mark <markmeldrum70@gmail.com>
2021-01-09 14:32:37 +0000

    Flight & Destination tests

--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -3,18 +3,24 @@ import java.util.ArrayList;
 public class Flight {
 
     private ArrayList<Passenger> bookedPassengers;
-    private Plane plane;
     private String flightNumber;
     private String destination;
     private String departureAirport;
     private String departureTime;
 
-    public Flight(Plane plane, String flightNumber, String destination, String departureAirport, String departureTime){
+    public Flight(String flightNumber, String destination, String departureAirport, String departureTime){
         this.bookedPassengers = new ArrayList<Passenger>();
-        this.plane = plane;
         this.flightNumber = flightNumber;
         this.destination = destination;
         this.departureAirport = departureAirport;
         this.departureTime = departureTime;
+    }
+
+    public String getFlightNumber() {
+        return flightNumber;
+    }
+
+    public String getDestination() {
+        return destination;
     }
 }
--- src/test/java/FlightTest.java
+++ src/test/java/FlightTest.java
@@ -1,2 +1,33 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 public class FlightTest {
+
+    Flight flight;
+    Plane plane;
+    Passenger passenger1;
+    Passenger passenger2;
+    Passenger passenger3;
+    Passenger passenger4; 
+    
+    @Before
+    public void setUp(){
+        flight = new Flight("BA1234", "PER", "EDI", "13:00");
+        plane = new Plane(PlaneType.CESSNA_182);
+        passenger1 = new Passenger("Mark", 2);
+        passenger2 = new Passenger("Chris", 1);
+        passenger3 = new Passenger("Michael", 3);
+        passenger4 = new Passenger("John", 2);
+    }
+    
+    @Test
+    public void canGetFlightNumber(){
+        assertEquals("BA1234", flight.getFlightNumber());
+    }
+
+    @Test
+    public void canGetDestination(){
+        assertEquals("PER", flight.getDestination());
+    }
 }
